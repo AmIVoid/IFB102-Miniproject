c5e42a682a13d24bcf7bf8b5c7eb545cf962fd17
AmIVoid <fukinvoid@gmail.com>
2021-05-17 20:02:18 +1000

    windows setup

---
+++ startup.bat
@@ -0,0 +1,9 @@
+echo "Creating required folders."
+mkdir downloaded_videos
+mkdir output_images
+mkdir uploaded_images
+echo "Folders created."
+echo ""
+echo "Installing required libraries."
+pip install -r requirements.txt
+echo "Required libraries installed."
